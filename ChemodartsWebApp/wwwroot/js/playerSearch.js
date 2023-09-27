13384fb20285ed7812699c5f508b86192a969c37
Ronny Hüttner <ronny.huettner@tu-dresden.de>
2023-09-27 13:15:59 +0200

    rolled out search for player- and Matchlist

--- ChemodartsWebApp/wwwroot/js/playerSearch.js
+++
@@ -1,8 +0,0 @@
-﻿$(document).ready(function () {
-    $("#playerSearch").on("input", function () {
-        var searchText = $(this).val().toLowerCase();
-        $("#playerList option").each(function () {
-            $(this).toggle($(this).text().toLowerCase().indexOf(searchText) > -1);
-        });
-    });
-});
---
+++ ChemodartsWebApp/wwwroot/js/search.js
@@ -0,0 +1,27 @@
+﻿$(document).ready(function () {
+    $("#playerSearch").on("input", function () {
+        var searchText = $(this).val().toLowerCase();
+        $("#playerList option").each(function () {
+            $(this).toggle($(this).text().toLowerCase().indexOf(searchText) > -1);
+        });
+    });
+});
+
+$(document).ready(function () {
+    $("#playerSearch").on("input", function () {
+        var searchText = $(this).val().toLowerCase();
+        $("#playerTable tr:not(:first)").filter(function () {
+            $(this).toggle($(this).text().toLowerCase().indexOf(searchText) > -1);
+        });
+    });
+});
+
+$(document).ready(function () {
+    $("#matchesSearch").on("input", function () {
+        var searchText = $(this).val().toLowerCase();
+        $("#matchesTable tr:not(:first)").filter(function () {
+            $(this).toggle($(this).text().toLowerCase().indexOf(searchText) > -1);
+        });
+    });
+});
+
