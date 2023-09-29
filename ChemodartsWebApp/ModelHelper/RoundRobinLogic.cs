37881ea5ce4c6dee0d67863ad73e5470bf68a8af
Ronny Hüttner <ronny.huettner@tu-dresden.de>
2023-09-29 14:42:25 +0200

    code cleanup and comments for MatchOrder Algorithm

--- ChemodartsWebApp/ModelHelper/RoundRobinLogic.cs
+++ ChemodartsWebApp/ModelHelper/RoundRobinLogic.cs
@@ -1,4 +1,5 @@
-﻿using ChemodartsWebApp.Data;
+﻿using Castle.Core.Internal;
+using ChemodartsWebApp.Data;
 using ChemodartsWebApp.Data.Factory;
 using ChemodartsWebApp.Models;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
@@ -34,6 +35,12 @@ namespace ChemodartsWebApp.ModelHelper
             }
         }
 
+        /// <summary>
+        /// CAUTION: This function has been carefully debugged and testet.
+        /// Even small changes will result in maybe completely unexspected behaviour.
+        /// Edit with care or leave as is!!
+        /// </summary>
+        /// <param name="r"></param>
         public static void UpdateMatchOrderInRound(Round r)
         {
             if (r is null || r.Modus != RoundModus.RoundRobin) return;
@@ -47,8 +54,8 @@ namespace ChemodartsWebApp.ModelHelper
             r.Groups.ToList().ForEach(g => allMatches.AddRange(g.Matches.Where(m => m.Group.Round.Equals(r))));
             allMatches.ForEach(m => m.MatchOrderValue = null);
 
-            //sort matches by matches per seed
-            List<SeedMatchCount> matchesPerSeed = allSeeds
+            //create container of matches per seed and sort them
+            List<SeedMatchCount> seedMatchCountList = allSeeds
                 .Select(s => new SeedMatchCount() { seed = s, cnt = s.Matches.Where(m => m.Group.Round.Equals(r)).Count() })
                 .OrderByDescending(mps => mps.cnt)
                 .ThenBy(mps => mps.seed.Matches.Min(m => m.MatchStage))
@@ -56,61 +63,73 @@ namespace ChemodartsWebApp.ModelHelper
 
             int matchOrder = 1;
             List<Seed> excludedSeeds = new List<Seed>();
-            for (int i = matchesPerSeed.Max(mps => mps.cnt); i > 0; i = matchesPerSeed.Max(mps => mps.cnt))
+
+            // run as long as at least one seed has more than one unordered match. Always start with highest match count (per seed)
+            // the function tries to give each seed a break as long as possible between two matches
+            for (int i = seedMatchCountList.Max(smc => smc.cnt); i > 0; i = seedMatchCountList.Max(smc => smc.cnt))
             {
-                List<Match> tmpMatches = new List<Match>();
-                bool stopExcluding = false;
-                //int excludeFromExcludeCnt = 1;
-                while (matchesPerSeed.Count(mps => mps.cnt == i) > 0)
+                List<Match> tmpMatches = new List<Match>(); //matches in the current tier
+                bool stopExcluding = false; //at some point it is necessary for a player to have multiple matches in one tier
+
+                //Update in tiers of seeds with same remaining match count
+                while (seedMatchCountList.Count(smc => smc.cnt == i) > 0)
                 {
                     //exclude seeds which are already been picked
-                    List<SeedMatchCount> tmpSMC = matchesPerSeed.Where(mps => mps.cnt == i && !excludedSeeds.Contains(mps.seed)).ToList();
+                    List<SeedMatchCount> tmpSMC = seedMatchCountList.Where(mps => mps.cnt == i && !excludedSeeds.Contains(mps.seed)).ToList();
                     if (tmpSMC.Count == 0)
                     {
                         //gradually remove seeds from excluded list when no more matches without seed duplication can be found
-                        stopExcluding = true;
+                        stopExcluding = true; // also stop excluding to prevent eternal loops
                         excludedSeeds.RemoveRange(0, Math.Min(1, excludedSeeds.Count));
                         continue;
                     }
 
-                    //get one seed with the highest matchCount
-                    SeedMatchCount? smc = tmpSMC.FirstOrDefault(mps => mps.cnt == i);
-                    if (smc is null) break;
-
-                    //Get the first match of that seed that has no match order yet
-                    Match? m = smc.seed.Matches.OrderBy(m => m.MatchStage).FirstOrDefault(m => m.MatchOrderValue is null);
-
-                    //Make sure each match gets picked only once
-                    if (m is object && allMatches.Contains(m))
+                    //get one of the seeds with the highest matchCount
+                    //SeedMatchCount? smc = tmpSMC.FirstOrDefault(mps => mps.cnt == i);
+                    foreach (SeedMatchCount smc in tmpSMC)
                     {
-                        //Make sure each seed gets picked only once in the same loop;
-                        int seedsOnEx = seedsOfMatchOnExclusionList(m, excludedSeeds);
-                        if (seedsOnEx == 0)
-                        {
-                            stopExcluding = false;
-                            SeedMatchCount? origSMC1 = matchesPerSeed.Find(x => x.seed.Equals(m.Seed1));
-                            SeedMatchCount? origSMC2 = matchesPerSeed.Find(x => x.seed.Equals(m.Seed2));
-                            if (origSMC1 is null || origSMC2 is null) return;
+                        if (smc is null) break;
 
-                            origSMC1.cnt -= 1;
-                            origSMC2.cnt -= 1;
-                            allMatches.Remove(m);
-                            tmpMatches.Add(m);
-                        }
-                        else if (stopExcluding && seedsOnEx == 1)
-                        {
-                            //To Prevent eternal loop remove 1 more
-                            excludedSeeds.RemoveRange(0, Math.Min(1, excludedSeeds.Count));
-                        }
+                        //Get the first match of that seed that has no match order yet
+                        Match? m = smc.seed.Matches.OrderBy(m => m.MatchStage).FirstOrDefault(m => m.MatchOrderValue is null);
 
-                        if (!stopExcluding)
+                        //Make sure each match gets picked only once
+                        if (m is object && allMatches.Contains(m))
                         {
-                            if (!excludedSeeds.Contains(m.Seed1)) excludedSeeds.Add(m.Seed1);
-                            if (!excludedSeeds.Contains(m.Seed2)) excludedSeeds.Add(m.Seed2);
+                            //Make sure each seed gets picked only once in the same loop
+                            int seedsOnEx = seedsOfMatchOnExclusionList(m, excludedSeeds);
+                            if (seedsOnEx == 0)
+                            {
+                                stopExcluding = false;
+                                SeedMatchCount? origSMC1 = seedMatchCountList.Find(x => x.seed.Equals(m.Seed1));
+                                SeedMatchCount? origSMC2 = seedMatchCountList.Find(x => x.seed.Equals(m.Seed2));
+                                if (origSMC1 is null || origSMC2 is null) return;
+
+                                origSMC1.cnt -= 1;
+                                origSMC2.cnt -= 1;
+                                allMatches.Remove(m);
+                                tmpMatches.Add(m);
+                            }
+
+                            //Exclude the players of the current mapped match for as long as possible
+                            if (!stopExcluding)
+                            {
+                                if (!excludedSeeds.Contains(m.Seed1)) excludedSeeds.Add(m.Seed1);
+                                if (!excludedSeeds.Contains(m.Seed2)) excludedSeeds.Add(m.Seed2);
+                                break; // break the tmpSMC foreach loop and update it with new exclusions
+                            }
                         }
+                    }
+
+                    if (tmpSMC.Count > 0 && stopExcluding)
+                    {
+                        // there is one match found, but all the opponents are still on the exclude list
+                        // remove gradually till two available players are found
+                        excludedSeeds.RemoveRange(0, Math.Min(1, excludedSeeds.Count));
                     }
                 }
 
+                //update the match order of the current tier and move to the next
                 tmpMatches.ToList().ForEach(m => {
                     m.MatchOrderValue = matchOrder++;
                 });
@@ -136,9 +155,17 @@ namespace ChemodartsWebApp.ModelHelper
             {
                 newMatches.AddRange(createMatches(g));
             }
+            if (newMatches.IsNullOrEmpty()) return;
+
             context.Matches.AddRange(newMatches);
             await context.SaveChangesAsync();
+
+            UpdateSeedsInRound(r);
+            UpdateMatchOrderInRound(r);
+            await context.SaveChangesAsync();
         }
+
+        #region Group Creator
 
         public static async Task<bool> CreateGroup(ChemodartsContext context, GroupFactoryRR factory, Round r, ModelStateDictionary? modelState)
         {
@@ -164,15 +191,19 @@ namespace ChemodartsWebApp.ModelHelper
                 context.MapperRP.AddRange(mappers);
                 await context.SaveChangesAsync();   
 
+                //create the matches
+                List<Match> newMatches = createMatches(g);
+                if(newMatches.IsNullOrEmpty()) throw new ArgumentNullException();
+
+                context.Matches.AddRange(newMatches);
+                await context.SaveChangesAsync();
+
                 UpdateSeedsInRound(r);
                 UpdateMatchOrderInRound(r);
 
-                //create the matches
-                List<Match> newMatches = createMatches(g);
-                context.Matches.AddRange(newMatches);
-
                 //Finally save to database
                 await context.SaveChangesAsync();
+
                 return true;
             }
             catch(ArgumentNullException e)
@@ -222,14 +253,14 @@ namespace ChemodartsWebApp.ModelHelper
             return mtsps;
         }
 
+        #endregion
+
         #region Match Creator
 
         private static List<Match> createMatches(Group g)
         {
             List<Match> matches = new List<Match>();
 
-            //List<Match> tmpList = scheduleRoundRobin(g.Seeds.ToList());
-            //List<Match> tmpList = scheduleRoundRobinManual(g.Seeds.ToList());
             List<Match> tmpList = GenerateRoundRobinMatchesCircleMethod(g.Seeds.ToList(), g.GroupHasRematch);
 
             tmpList.ForEach(m => {
@@ -283,34 +314,6 @@ namespace ChemodartsWebApp.ModelHelper
                 }
             }
 
-            return matches;
-        }
-
-        private static List<Match> scheduleRoundRobinManual(List<Seed> seeds)
-        {
-            List<Match> matches = new List<Match>();
-            if (seeds.Count == 3)
-            {
-                matches.Add(new Match() { Seed1Id = seeds.ElementAt(0).SeedId, Seed2Id = seeds.ElementAt(1).SeedId, MatchOrderValue = 0 });
-                matches.Add(new Match() { Seed1Id = seeds.ElementAt(1).SeedId, Seed2Id = seeds.ElementAt(2).SeedId, MatchOrderValue = 1 });
-                matches.Add(new Match() { Seed1Id = seeds.ElementAt(0).SeedId, Seed2Id = seeds.ElementAt(2).SeedId, MatchOrderValue = 2 });
-
-                matches.Add(new Match() { Seed1Id = seeds.ElementAt(2).SeedId, Seed2Id = seeds.ElementAt(1).SeedId, MatchOrderValue = 3 });
-                matches.Add(new Match() { Seed1Id = seeds.ElementAt(2).SeedId, Seed2Id = seeds.ElementAt(0).SeedId, MatchOrderValue = 4 });
-                matches.Add(new Match() { Seed1Id = seeds.ElementAt(1).SeedId, Seed2Id = seeds.ElementAt(0).SeedId, MatchOrderValue = 5 });
-            }
-            else if (seeds.Count == 4)
-            {
-                matches.Add(new Match() { Seed1Id = seeds.ElementAt(0).SeedId, Seed2Id = seeds.ElementAt(1).SeedId, MatchOrderValue = 1 });
-                matches.Add(new Match() { Seed1Id = seeds.ElementAt(2).SeedId, Seed2Id = seeds.ElementAt(3).SeedId, MatchOrderValue = 1 });
-
-                matches.Add(new Match() { Seed1Id = seeds.ElementAt(0).SeedId, Seed2Id = seeds.ElementAt(2).SeedId, MatchOrderValue = 3 });
-                matches.Add(new Match() { Seed1Id = seeds.ElementAt(1).SeedId, Seed2Id = seeds.ElementAt(3).SeedId, MatchOrderValue = 3 });
-
-                matches.Add(new Match() { Seed1Id = seeds.ElementAt(0).SeedId, Seed2Id = seeds.ElementAt(3).SeedId, MatchOrderValue = 5 });
-                matches.Add(new Match() { Seed1Id = seeds.ElementAt(1).SeedId, Seed2Id = seeds.ElementAt(2).SeedId, MatchOrderValue = 5 });
-
-            }
             return matches;
         }
 
